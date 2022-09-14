10e9eb0f441324b61fec3d40dab8b0ed192eecbf
Wilson Ibekason <wanbekgithub@gmail.com>
2022-09-14 13:30:48 +0100

    complete add artist section

--- src/components/details/Artist.jsx
+++ src/components/details/Artist.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import styles, { Layout } from "../../styles/AppdeyCustomStyles";
 
 const Artist = () => {
-  return <div className="text-center">Artist</div>;
+  const { BannerGridChild, BannerGridChild2, BannerGridLayout } = Layout;
+  const {
+    flexCol,
+    flexCentered,
+    flexStart,
+    paragraphCustom,
+    ButtonStyle,
+    transitions,
+  } = styles;
+  return (
+    <>
+      <div className={`my-10`}>
+        <div className={`${BannerGridLayout} `}>
+          <div className={`${BannerGridChild}`}>
+            <div className={`h-[500px]`}>
+              <img
+                src="https://cdn.shopify.com/s/files/1/0425/2542/5825/collections/IMG_7912.jpg?v=1661244261&width=535"
+                alt=""
+                className="w-full h-full object-cover max-w-full "
+                width={500}
+                height={450}
+              />
+            </div>
+          </div>
+          {/* card contents */}
+          <div className={`${BannerGridChild2} bg-gradientBaseAccent relative`}>
+            <div className={`inset-0 m-auto absolute w-[80%] h-[80%]`}>
+              <div className={`${flexCol} ${flexStart} max-w-[80%] space-y-3`}>
+                <h5
+                  className={`${paragraphCustom} text-gray-600 text-sm capitalize`}
+                >
+                  artist
+                </h5>
+                <h4
+                  className={`${paragraphCustom} text-gray-400 text-md uppercase`}
+                >
+                  Ebeneza
+                </h4>
+                <p
+                  className={`${paragraphCustom} text-lg text-gray-300 tracking-wide leading-8`}
+                >
+                  Ebenezer Ali known as Eben or Ckle a Ghanaian artist born on
+                  10th December 1992 in Accra (Ghana). He graduated as a Visual
+                  Artist with a special interest in the theoretical aspect of
+                  art . He then continued by learning fine art and designing
+                  through apprenticeship for 3 years. Eben has a passion for
+                  painting, through which he discovered a different style from
+                  which he was used to, abstract art. In 2018 He started selling
+                  some of these paintings to galleries such as Accra Art Centre,
+                  African Market gallery and the Loom gallery all in Accra
+                  Ghana.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* banner bottom */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Artist;
